ac2603f873d3e7ff18dab612c377423effefd474
Danil Deineka <tripledpadik@gmail.com>
2024-01-09 18:51:37 +0200

    Make activity page

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -2,8 +2,10 @@
 
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 import { db } from "@/lib/db";
+import { createAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
 
 import { CreateCard } from "./schema";
@@ -47,6 +49,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
 
     card = await db.card.create({
       data: { title, listId, order: newOrder },
+    });
+
+    await createAuditLog({
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
     });
   } catch (error) {
     return {
